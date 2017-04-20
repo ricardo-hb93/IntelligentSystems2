ac7db2ecc973ebca64f8ac7ad03c6b621cd6dc02
eseuteo <ricardoholthausen@gmail.com>
2017-04-20 11:55:54 +0200

    Add files via upload
    SuperTracker now reads from file

---
+++ SuperTracker.java
@@ -0,0 +1,126 @@
+package supersample;
+
+import robocode.*;
+import java.awt.*;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * SuperTracker - a Super Sample Robot by CrazyBassoonist based on the robot
+ * Tracker by Mathew Nelson and maintained by Flemming N. Larsen
+ * <p/>
+ * Locks onto a robot, moves close, fires when close.
+ */
+public class SuperTracker extends AdvancedRobot {
+	int moveDirection = 1;// which way to move
+	private double dist;
+	private double speedP;
+	private double range;
+	private double minSpeed;
+	/**
+	 * run:  Tracker's main run function
+	 */
+	public void run() {
+		tuneBot("put your path here");
+		setAdjustRadarForRobotTurn(true);//keep the radar still while we turn
+		setBodyColor(new Color(128, 128, 50));
+		setGunColor(new Color(50, 50, 20));
+		setRadarColor(new Color(200, 200, 70));
+		setScanColor(Color.white);
+		setBulletColor(Color.blue);
+		setAdjustGunForRobotTurn(true); // Keep the gun still when we turn
+		turnRadarRightRadians(Double.POSITIVE_INFINITY);//keep turning radar right
+		}
+
+	/**
+	 * onScannedRobot: Here's the good stuff
+	 */
+	public void onScannedRobot(ScannedRobotEvent e) {
+		double absBearing = e.getBearingRadians() + getHeadingRadians();// enemies
+																		// absolute
+																		// bearing
+		double latVel = e.getVelocity() * Math.sin(e.getHeadingRadians() - absBearing);// enemies
+																						// later
+																						// velocity
+		double gunTurnAmt;// amount to turn our gun
+		setTurnRadarLeftRadians(getRadarTurnRemainingRadians());// lock on the
+																// radar
+		if (Math.random() > speedP) {
+			setMaxVelocity((range * Math.random()) + minSpeed);// randomly change speed
+														// \\ VELOCIDAD + RANGO
+														// + MINIMA VELOCIDAD
+		}
+		if (e.getDistance() > dist) {// if distance is greater than 150 \\
+									// DISTANCIA
+			gunTurnAmt = robocode.util.Utils.normalRelativeAngle(absBearing - getGunHeadingRadians() + latVel / 22);// amount
+																													// to
+																													// turn
+																													// our
+																													// gun,
+																													// lead
+																													// just
+																													// a
+																													// little
+																													// bit
+			setTurnGunRightRadians(gunTurnAmt); // turn our gun
+			setTurnRightRadians(
+					robocode.util.Utils.normalRelativeAngle(absBearing - getHeadingRadians() + latVel / getVelocity()));// drive
+																														// towards
+																														// the
+																														// enemies
+																														// predicted
+																														// future
+																														// location
+			setAhead((e.getDistance() - 140) * moveDirection);// move forward
+			setFire(3);// fire
+		} else {// if we are close enough...
+			gunTurnAmt = robocode.util.Utils.normalRelativeAngle(absBearing - getGunHeadingRadians() + latVel / 15);// amount
+																													// to
+																													// turn
+																													// our
+																													// gun,
+																													// lead
+																													// just
+																													// a
+																													// little
+																													// bit
+			setTurnGunRightRadians(gunTurnAmt);// turn our gun
+			setTurnLeft(-90 - e.getBearing()); // turn perpendicular to the
+												// enemy
+			setAhead((e.getDistance() - 140) * moveDirection);// move forward
+			setFire(3);// fire
+		}
+	}
+
+	public void onHitWall(HitWallEvent e) {
+		moveDirection = -moveDirection;// reverse direction upon hitting a wall
+	}
+
+	/**
+	 * onWin: Do a victory dance
+	 */
+	public void onWin(WinEvent e) {
+		for (int i = 0; i < 50; i++) {
+			turnRight(30);
+			turnLeft(30);
+		}
+	}
+
+	private void tuneBot(String path) {
+		try {
+			Scanner sc = new Scanner(new FileReader(path));
+			
+			sc.useDelimiter("[<>]+");
+			dist = sc.nextDouble();
+			speedP = sc.nextDouble();
+			range = sc.nextDouble();
+			minSpeed = sc.nextDouble();
+			
+			
+			sc.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
