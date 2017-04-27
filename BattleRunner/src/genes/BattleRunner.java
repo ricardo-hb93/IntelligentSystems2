443967c57670c99654ec834b016a17d397242b4e
MrYompi <Ojedamorala@hotmail.com>
2017-04-27 12:28:13 +0200

    Add files via upload

---
+++ BattleRunner/src/genes/BattleRunner.java
@@ -0,0 +1,66 @@
+package genes;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+import org.jgap.Chromosome;
+import org.jgap.Configuration;
+import org.jgap.InvalidConfigurationException;
+import org.jgap.impl.DoubleGene;
+
+import robocode.control.*;
+import robocode.control.events.*;
+
+public class BattleRunner {
+	static double INIT_X = 32.0;
+	static double INIT_Y = 32.0;
+	static int results;
+	public static int run() {
+		RobocodeEngine.setLogMessagesEnabled(false);
+		RobocodeEngine engine = new RobocodeEngine(new java.io.File("C:/Robocode"));
+		engine.addBattleListener(new BattleObserver());
+		engine.setVisible(true);
+
+		long inactivityTime = 10000000;
+		double gunCoolingRate = 0.2;
+		int sentryBorderSize = 50;
+		boolean hideEnemyNames = false;
+		int numRounds = 5;
+
+		BattlefieldSpecification battlefield = new BattlefieldSpecification(832, 832);
+		RobotSpecification[] selectedRobots = new RobotSpecification[2];
+		RobotSetup[] initialSetups = new RobotSetup[2];
+		selectedRobots = engine.getLocalRepository("supersample.SuperRamFire*,supersample.SuperTracker*");
+		initialSetups[0] = new RobotSetup(580.0, 580.0, 0.0);
+		initialSetups[1] = new RobotSetup(500.0, 500.0, 0.0);
+		BattleSpecification battleSpec = new BattleSpecification(battlefield, numRounds, inactivityTime, gunCoolingRate,
+				sentryBorderSize, hideEnemyNames, selectedRobots, initialSetups);
+		engine.runBattle(battleSpec, true);
+		engine.close();
+		System.out.println(results);
+		return results;
+		
+	}
+	public static void setResults(int r){
+		results=r;
+	}
+}
+
+class BattleObserver extends BattleAdaptor {
+
+	public void onBattleCompleted(BattleCompletedEvent e) {
+		System.out.println("-- Battle has completed --");
+
+		System.out.println("Battle results:");
+		BattleRunner.setResults(e.getSortedResults()[0].getScore()-e.getSortedResults()[1].getScore());
+	}
+
+	public void onBattleMessage(BattleMessageEvent e) {
+		System.out.println("Msg> " + e.getMessage());
+	}
+
+	public void onBattleError(BattleErrorEvent e) {
+		System.out.println("Err> " + e.getError());
+	}
+}
---
+++ BattleRunner/src/genes/Main.java
@@ -0,0 +1,38 @@
+package genes;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+import org.jgap.Chromosome;
+import org.jgap.Configuration;
+import org.jgap.InvalidConfigurationException;
+import org.jgap.impl.DoubleGene;
+
+public class Main {
+	public static void main(String[]args){
+		DoubleGene genes[] = new DoubleGene[4];
+		try {
+			int i = 0;
+			Configuration conf=new Configuration();
+			Scanner sc = new Scanner(new FileReader("C:\\Users\\alumno\\workspace-jee\\BattleRunner\\src\\a.txt"));
+			Scanner sc2 = new Scanner(sc.nextLine());
+
+			while (i<4) {
+				genes[i] = new DoubleGene(conf);
+				genes[i].setAllele(sc2.nextDouble());
+				System.out.println(genes[i].doubleValue());
+				i++;
+			}
+			Chromosome c = new Chromosome(conf, genes);
+			sc2.close();
+			sc.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InvalidConfigurationException e) {
+			System.out.println("Meh");
+		}
+		BattleRunner.run();
+		System.exit(0);
+	}
+}
